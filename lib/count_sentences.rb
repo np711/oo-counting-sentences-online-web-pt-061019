e4c9f1d758751af3d0ac5f81e90ac7d55fa9567e
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-26 21:00:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence? do
-    it "returns true if the string that you are calling this methid on "
+    it "returns true if the string that you are calling this methid on ends in "
     self.end_with?(".") ? true : false
     
   end
